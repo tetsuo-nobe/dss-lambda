210a828cb4081c399a27c32f142bcf817b0a7eeb
Workshop Participant <participant@workshops.aws>
2025-08-18 22:50:43 +0000

    modify app.py

--- demo_function/app.py
+++ demo_function/app.py
@@ -1,10 +1,20 @@
 import json
 import time
+from datetime import datetime
 
 # ハンドラ関数の外側で3秒間sleep
 #time.sleep(3)
+# ハンドラ関数の外側で日時データを取得
+#outside_handler = datetime.now()
 
 def lambda_handler(event, context):
+    # ハンドラ関数の内側で日時データを取得
+    inside_handler = datetime.now()  
+    # 取得した日時データをログ出力
+    print('outside_handler :' + str(outside_handler))
+    print('inside_handler  :' + str(inside_handler))
+
+    # HTML コンテンツの出力
     html_content = """
     <!DOCTYPE html>
     <html>
