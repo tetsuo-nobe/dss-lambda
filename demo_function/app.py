5cfe1b96c2662c1e66d10b931172c8a48d1c04ba
Tetsuo Nobe <69065590+tetsuo-nobe@users.noreply.github.com>
2025-08-19 10:39:01 +0900

    Update app.py

--- demo_function/app.py
+++ demo_function/app.py
@@ -11,7 +11,7 @@ def lambda_handler(event, context):
     # ハンドラ関数の内側で日時データを取得
     inside_handler = datetime.now()  
     # 取得した日時データをログ出力
-    print('outside_handler :' + str(outside_handler))
+    #print('outside_handler :' + str(outside_handler))
     print('inside_handler  :' + str(inside_handler))
 
     # HTML コンテンツの出力
